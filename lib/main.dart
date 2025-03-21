8d9f8fff05f3afdfe32bafa6dce2563032304e2e
windows <wilsonvargad98@gmail.com>
2025-03-20 20:26:12 -0600

    Un arreglo para proximas modificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon College Deals',
       debugShowCheckedModeBanner: false,
-      home: BargainsFinds(),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFE3E6E6),
+        body: Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(children: [BargainsFinds()]),
+          ),
+        ),
+      ),
     );
   }
 }
